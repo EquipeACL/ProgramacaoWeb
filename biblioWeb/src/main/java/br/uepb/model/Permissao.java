4d4df683fa1e6193322ae83a1b23b52000b08055
Caio Lucena <caiolucena.cc@gmail.com>
2018-05-11 12:48:34 -0300

    Alterando permissao

--- biblioWeb/src/main/java/br/uepb/model/Permissao.java
+++ biblioWeb/src/main/java/br/uepb/model/Permissao.java
@@ -26,6 +26,28 @@ public class Permissao {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Permissao other = (Permissao) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 	
 	
 	
