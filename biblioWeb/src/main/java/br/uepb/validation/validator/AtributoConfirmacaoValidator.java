6a763407d8206d319108417ed971206687d2ab8c
Caio Lucena <caiolucena.cc@gmail.com>
2018-05-16 18:51:26 -0300

    Atualizando o Pomxml

--- biblioWeb/src/main/java/br/uepb/validation/validator/AtributoConfirmacaoValidator.java
+++ biblioWeb/src/main/java/br/uepb/validation/validator/AtributoConfirmacaoValidator.java
@@ -2,27 +2,33 @@ package br.uepb.validation.validator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
+import org.apache.commons.beanutils.*;
 import br.uepb.validation.AtributoConfirmacao;
 
-public class AtributoConfirmacaoValidator implements ConstraintValidator<AtributoConfirmacao,Object>{
+public class AtributoConfirmacaoValidator implements ConstraintValidator<AtributoConfirmacao, Object> {
 
 	private String atributo;
 	private String atributoConfirmacao;
-	
+
 	@Override
 	public void initialize(AtributoConfirmacao constraintAnnotation) {
-		
+
 		this.atributo = constraintAnnotation.atributo();
 		this.atributoConfirmacao = constraintAnnotation.atributoConfirmacao();
-		
+
 	}
 
 	@Override
 	public boolean isValid(Object object, ConstraintValidatorContext arg1) {
-		Object valorAtributo;
+		 
+		try {
+		Object valorAtributo = BeanUtils.getProperty(object, this.atributo);
+		Object valorAtributoConfirmacao = BeanUtils.getProperty(object, this.atributoConfirmacao);
 		
-		//Aula 18.4 11:48 
+		//Aula 18.4 11:48
+		}catch(Exception e) {
+			throw new RuntimeException("Erro recuperando valores dos atributos",e);
+		}
 		
 		return false;
 	}
