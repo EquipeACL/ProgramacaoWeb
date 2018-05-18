4c94d3a61854148ad817d338141fd4d9d7bc17f6
lucas <lucas@DESKTOP-RL0C72Q>
2018-05-18 11:10:16 -0300

    Javadoc do pacote biblio.config.init

--- biblioWeb/src/main/java/br/uepb/biblio/config/init/AppInitializer.java
+++ biblioWeb/src/main/java/br/uepb/biblio/config/init/AppInitializer.java
@@ -9,24 +9,41 @@ import br.uepb.biblio.config.JPAConfig;
 import br.uepb.biblio.config.ServiceConfig;
 import br.uepb.biblio.config.WebConfig;
 
+/**
+ * Essa classe é responsável pelas configurações de inicialização. Serve para designar onde estão as configurações, em ordem de prioridade..
+ * @author EquipeACL
+ *
+ */
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 		
+	/**
+	 * Esse método é responsável por configurar o JPA e o ServiceConfig.
+	 */
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		//return new Class<?>[] { H2Config.class, ServiceConfig.class };
 		return new Class<?>[] { JPAConfig.class, ServiceConfig.class };
 	}
 
+	/**
+	 * Esse método é responsável por configurar o WebConfig.
+	 */
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class<?>[] { WebConfig.class };
 	}
-
+	
+	/**
+	 * Esse método é responsável por realizar o mapeamento dos endereços das views pelas barras.
+	 */
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
 	}
-
+	
+	/**
+	 * Esse método é responsável por definir a configuração da codificação de caracteres utilizadas na view.
+	 */
 	@Override
 	protected Filter[] getServletFilters() {
 		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
