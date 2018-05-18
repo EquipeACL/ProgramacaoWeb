7dbe23f3d0c48e6e508b01e6d6bd96c925b184f7
Caio Lucena <caiolucena.cc@gmail.com>
2018-05-18 14:08:38 -0300

    Configurações do Security, tela de login

--- biblioWeb/src/main/java/br/uepb/biblio/config/SecurityConfig.java
+++ biblioWeb/src/main/java/br/uepb/biblio/config/SecurityConfig.java
@@ -22,10 +22,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
+			.antMatchers("/layout/**").permitAll()
+			.antMatchers("/images/**").permitAll()
 			.anyRequest().authenticated()
 			.and()
 			.formLogin()
-			.and()
+				.loginPage("/login")
+				.permitAll()
+				.and()
 			.csrf().disable(); 
 		
 		
---
+++ biblioWeb/src/main/java/br/uepb/biblio/controller/SegurancaController.java
@@ -0,0 +1,15 @@
+package br.uepb.biblio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SegurancaController {
+
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+}
