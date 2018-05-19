77f02b0927f6c0ffd64c82a15a715f701740c64c
lucas <lucas@DESKTOP-R7RBKBO>
2018-05-18 23:56:55 -0300

    Teste Funcionario

--- biblioWeb/src/test/java/testes/TesteCaseFuncionario.java
+++ biblioWeb/src/test/java/testes/TesteCaseFuncionario.java
@@ -1,110 +1,62 @@
-//package testes.usuarios;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import br.uepb.model.usuarios.Administrador;
-//import br.uepb.model.usuarios.Funcionario;
-//
-//public class TesteCaseFuncionario {
-//	private Funcionario funcionario;
-//	private Administrador administrador;
-//	
-//	@Before
-//	public void setup() {
-//		funcionario = new Funcionario();
-//		administrador = new Administrador();
-//	} 
-//	
-//	@Test
-//	public void testeCreateFuncionario() {
-//		funcionario = new Funcionario(123,"Funcionario1",123,"Brasil","Rua da ladeira",1234,"email@gmail.com","senha123","func1");
-//		assertTrue(administrador.createFuncionario(funcionario));
-//		funcionario = administrador.searchFuncionario("Funcionario1").get(0);
-//		assertTrue(administrador.removeFuncionario(funcionario));
-//	}
-//	
-//	@Test
-//	public void testeRemoveFuncionario() {
-//		funcionario = new Funcionario(123,"Funcionario1",123,"Brasil","Rua da ladeira",1234,"email@gmail.com","senha123","func1");
-//		assertTrue(administrador.createFuncionario(funcionario));
-//		funcionario = new Funcionario(123,"Funcionario2",123,"Brasil","Rua da ladeira",1234,"email@gmail.com","senha123","func2");
-//		assertTrue(administrador.createFuncionario(funcionario));
-//		funcionario = new Funcionario(123,"Funcionario3",123,"Brasil","Rua da ladeira",1234,"email@gmail.com","senha123","func3");
-//		assertTrue(administrador.createFuncionario(funcionario));
-//		funcionario.setNomeCompleto("Funcionario");		
-//		for(Funcionario f:administrador.searchFuncionario("Funcionario")) {
-//			assertTrue(administrador.removeFuncionario(f));
-//		}
-//	}
-//	
-//	@Test
-//	public void testeUpdateFuncionario() {
-//		funcionario = new Funcionario(123,"Funcionario1",123,"Brasil","Rua da ladeira",1234,"email@gmail.com","senha123","func1");
-//		assertTrue(administrador.createFuncionario(funcionario));
-//		funcionario = administrador.searchFuncionario("Funcionario1").get(0);
-//		funcionario.setNomeCompleto("Novo Nome");
-//		funcionario.setEmail("novoemail@gmail.com");
-//		assertTrue(administrador.updateFuncionario(funcionario));
-//	}
-//	
-//	@Test
-//	public void testeSearchFuncionario() {
-//		funcionario = new Funcionario(123,"Funcionario1",123,"Brasil","Rua da ladeira",1234,"email@gmail.com","senha123","func1");
-//		assertTrue(administrador.createFuncionario(funcionario));
-//		
-//		funcionario = administrador.searchFuncionario("Funcionario1").get(0);
-//		assertEquals("Funcionario1",funcionario.getNomeCompleto());
-//		assertEquals(123,funcionario.getCpf());
-//		assertEquals("Rua da ladeira",funcionario.getEndereco());
-//		assertEquals("email@gmail.com",funcionario.getEmail());
-//		assertEquals("senha123",funcionario.getSenhaAcesso());
-//		assertEquals("func1",funcionario.getNomeUsuario());
-//	}
-//	
-//	@Test
-//	public void testeValidaFuncionario(){
-//		funcionario.setCpf(0);
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setCpf(123);
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setEmail("");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setEmail("email@gmail.com");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setEndereco("");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setEndereco("Rua da laderia");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setNaturalidade("");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setNaturalidade("Brasil");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setNomeCompleto("");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setNomeCompleto("Funcionario");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setNomeUsuario("");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setNomeUsuario("func");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setRg(-1);
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setRg(123);
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setSenhaAcesso("");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setSenhaAcesso("senha");
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setTelefone(-1);
-//		assertFalse(administrador.createFuncionario(funcionario));
-//		funcionario.setTelefone(3333);
-//		assertTrue(administrador.createFuncionario(funcionario));
-//		for(Funcionario f: administrador.searchFuncionario("Funcionario")) {
-//			assertTrue(administrador.removeFuncionario(f));
-//		}
-//		
-//	}
-//}
+package testes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+
+import br.uepb.biblio.config.H2Config;
+import br.uepb.biblio.config.SecurityConfig;
+import br.uepb.biblio.config.ServiceConfig;
+import br.uepb.biblio.config.init.AppInitializer;
+import br.uepb.biblio.service.CadastroFuncionarioService;
+import br.uepb.model.usuarios.Funcionario;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppInitializer.class,H2Config.class,ServiceConfig.class,SecurityConfig.class})
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class,
+        DbUnitTestExecutionListener.class,
+WithSecurityContextTestExecutionListener.class})
+public class TesteCaseFuncionario {
+	private Funcionario funcionario;
+	
+	
+	@Autowired
+	private CadastroFuncionarioService funcionarioDAO;
+	
+	@Before
+	public void setup() {
+		funcionario = new Funcionario();
+
+	} 
+	
+	@Test
+	public void testeCreateFuncionario() {
+		funcionario = new Funcionario("123.040.484-33", "Funcionario1", "34655", "Brasil", "Rua da ladeira", "(83) 99864-6328", "lucas", "123", "lucas", "123");
+		funcionario.setLogin("lucas");
+		assertTrue(funcionarioDAO.salvar(funcionario)!=null);
+		funcionarioDAO.remover(funcionarioDAO.buscarPorNome("Funcionario1").get(0).getId());
+	}
+	
+	@Test
+	public void testeRemoveFuncionario() {
+		funcionario = new Funcionario("123.040.484-21", "Funcionario2", "34656", "Brasil", "Rua da ladeira", "(83) 99864-6328", "joaozinho", "123", "joaozinho", "123");
+		funcionario.setLogin("joaozinho");
+		assertTrue(funcionarioDAO.salvar(funcionario)!=null);
+		assertTrue(funcionarioDAO.remover(funcionarioDAO.buscarPorNome("Funcionario2").get(0).getId()));
+	}
+	
+}
